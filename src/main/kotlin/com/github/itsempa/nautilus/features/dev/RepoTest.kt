95016335daf3033ee96282356c06e98d173c2fc6
Empa <itsempa@users.noreply.github.com>
2025-04-15 17:21:01 +0200

    feat: add repo test

---
+++ src/main/kotlin/com/github/itsempa/nautilus/features/dev/RepoTest.kt
@@ -0,0 +1,41 @@
+package com.github.itsempa.nautilus.features.dev
+
+import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.utils.NeuInternalName
+import com.github.itsempa.nautilus.data.repo.FishingDropsRepo
+import com.github.itsempa.nautilus.events.NautilusCommandRegistrationEvent
+import com.github.itsempa.nautilus.events.NautilusRepositoryReloadEvent
+import com.github.itsempa.nautilus.modules.Module
+import com.github.itsempa.nautilus.utils.NautilusChatUtils
+
+@Module(devOnly = true)
+object RepoTest {
+
+    private var items = mapOf<NeuInternalName, List<String>>()
+
+    @HandleEvent
+    fun onRepoReload(event: NautilusRepositoryReloadEvent) {
+        items = event.getConstant<FishingDropsRepo>("FishingDrops").items
+    }
+
+    private fun sendTest() {
+        val msg = items.entries.joinToString { (internalName, mobs) ->
+            var text = "${internalName.asString()}:\n"
+            mobs.forEach { mob ->
+                text += "  - $mob\n"
+            }
+            text
+        }
+        NautilusChatUtils.chat(msg)
+    }
+
+    @HandleEvent
+    fun onCommandRegistration(event: NautilusCommandRegistrationEvent) {
+        event.register("ntrepotest") {
+            this.callback {
+                sendTest()
+            }
+        }
+    }
+
+}
