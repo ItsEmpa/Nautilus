714ea82ebc5434c7db54f8239211d62186690acd
Empa <itsempa@users.noreply.github.com>
2025-04-22 16:17:08 +0200

    feat: add most of the FishingCategories (not the detection), and recursively load all Fishing Categories.

--- src/main/kotlin/com/github/itsempa/nautilus/data/categories/FishingCategory.kt
+++ src/main/kotlin/com/github/itsempa/nautilus/data/categories/FishingCategory.kt
@@ -1,73 +1,125 @@
 package com.github.itsempa.nautilus.data.categories
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
-import at.hannibal2.skyhanni.config.core.config.Position
+import at.hannibal2.skyhanni.config.commands.CommandCategory
 import at.hannibal2.skyhanni.data.IslandType
-import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.SecondPassedEvent
 import at.hannibal2.skyhanni.events.fishing.FishingBobberInLiquidEvent
 import at.hannibal2.skyhanni.features.fishing.FishingApi
+import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.LorenzUtils.isInIsland
-import at.hannibal2.skyhanni.utils.RenderUtils.renderString
+import at.hannibal2.skyhanni.utils.collection.CollectionUtils.addNotNull
 import com.github.itsempa.nautilus.events.NautilusCommandRegistrationEvent
 import com.github.itsempa.nautilus.modules.Module
+import kotlin.reflect.KClass
 
-sealed class FishingCategory(val internalName: String, private val parent: FishingCategory? = null) {
+@Suppress("unused")
+sealed class FishingCategory(val internalName: String, private val canDuplicate: Boolean = false) {
+    private var parent: FishingCategory? = null
     private val children: MutableList<FishingCategory> = mutableListOf()
 
     fun isActive() = (parent?.checkActive() ?: true) && checkActive()
     internal open fun checkActive(): Boolean = false
 
-    init {
-        @Suppress("LeakingThis")
-        parent?.children?.add(this)
-        @Suppress("LeakingThis")
-        categories.add(this)
-    }
-
-    @Module
-    data object Lava : FishingCategory("LAVA", null) {
+    data object Lava : FishingCategory("LAVA") {
         override fun checkActive(): Boolean = lastWater == false
 
-        @Module
-        data object CrimsonIsles : FishingCategory("CRIMSON_ISLE", Lava) {
+        data object CrimsonIsle : FishingCategory("CRIMSON_ISLE") {
             override fun checkActive(): Boolean = IslandType.CRIMSON_ISLE.isInIsland()
 
-            @Module
-            data object TrophyFish : FishingCategory("TROPHY_FISH", CrimsonIsles) {
+            data object TrophyFish : FishingCategory("TROPHY_FISH") {
                 override fun checkActive(): Boolean = FishingApi.wearingTrophyArmor
             }
 
-            @Module
-            data object CrimsonIsleFish : FishingCategory("CI_FISH", CrimsonIsles) {
+            data object Fish : FishingCategory("CI_FISH") {
                 override fun checkActive(): Boolean = !TrophyFish.checkActive()
 
-                @Module
-                data object CrimsonIsleHotspot : FishingCategory("CI_HOTSPOT", CrimsonIsles) {
+                data object Hotspot : FishingCategory("CI_HOTSPOT") {
                     override fun checkActive(): Boolean = false // TODO: Implement Fishing in Hotsspot detection
                 }
             }
         }
 
-        @Module
-        data object CrystalHollows : FishingCategory("CRYSTAL_HOLLOWS", Lava) {
+        data object CrystalHollows : FishingCategory("CH_LAVA") {
             override fun checkActive(): Boolean = IslandType.CRYSTAL_HOLLOWS.isInIsland()
 
-            @Module
-            data object MagmaCore : FishingCategory("MAGMA_CORE", CrystalHollows)
+            data object MagmaCore : FishingCategory("MAGMA_CORE")
 
-            @Module
-            data object Worms : FishingCategory("CH_WORMS", CrystalHollows)
+            data object Worms : FishingCategory("CH_LAVA_WORMS")
+        }
+    }
+
+    data object Water : FishingCategory("WATER") {
+        override fun checkActive(): Boolean = lastWater == true
+
+        data object CrystalHollows : FishingCategory("CH_WATER") {
+            override fun checkActive(): Boolean = IslandType.CRYSTAL_HOLLOWS.isInIsland()
+
+            data object Worm : FishingCategory("CH_WATER_WORM")
+
+            data object AbyssalMines : FishingCategory("ABYSSAL_MINES")
+        }
+
+        data object DwarvenMines : FishingCategory("DW_WATER") {
+            override fun checkActive(): Boolean = IslandType.DWARVEN_MINES.isInIsland()
+
+            data object Grubbers : FishingCategory("DW_WATER_GRUBBERS")
+        }
+
+        data object Park : FishingCategory("PARK") {
+            override fun checkActive(): Boolean = IslandType.THE_PARK.isInIsland()
+
+            data object InkFishing : FishingCategory("INK_FISHING")
+        }
+
+        data object Hotspot : FishingCategory("HOTSPOT") {
+            override fun checkActive(): Boolean = false // TODO: Implement Fishing in Hotspot detection
+
+            data object Bayou : FishingCategory("BAYOU") {
+                override fun checkActive(): Boolean = IslandType.BACKWATER_BAYOU.isInIsland()
+            }
+        }
+
+        data object Oasis : FishingCategory("OASIS") {
+            override fun checkActive(): Boolean = IslandType.THE_FARMING_ISLANDS.isInIsland()
+        }
+
+        object Events {
+            data object Spooky : FishingCategory("SPOOKY", true) {
+                override fun checkActive(): Boolean = false // TODO: Implement Spooky Fishing detection
+            }
+
+            data object Jerry : FishingCategory("JERRY", true) {
+                override fun checkActive(): Boolean = false // TODO: Implement Jerry Fishing detection
+            }
+
+            data object FishingFestival : FishingCategory("FISHING_FESTIVAL", true) {
+                override fun checkActive(): Boolean = false // TODO: Implement Fishing Festival detection
+            }
         }
     }
 
     @Module
     companion object {
-        val categories: MutableList<FishingCategory> = mutableListOf()
-        val parentCategories: List<FishingCategory> get() = categories.filter { it.parent == null }
+        val categories: List<FishingCategory>
+        private val extraCategories: List<FishingCategory>
+        private val parentCategories: List<FishingCategory>
         private var lastWater: Boolean? = null
         var activeCategory: FishingCategory? = null
             private set
+
+        // TODO: Implement having multiple categories at once
+        var activeCategories: Set<FishingCategory> = emptySet()
+            private set
+
+        init {
+            categories = buildList {
+                recursiveLoad(Lava::class)
+                recursiveLoad(Water::class)
+            }
+            parentCategories = categories.filter { it.parent == null }
+            extraCategories = categories.filter { it.canDuplicate }
+        }
 
         fun getCategoryByName(name: String): FishingCategory? = categories.find { it.internalName == name }
 
@@ -79,13 +131,10 @@ sealed class FishingCategory(val internalName: String, private val parent: Fishi
         @HandleEvent
         fun onCommand(event: NautilusCommandRegistrationEvent) {
             event.register("ntfishingcategorytest") {
+                this.description = "Prints the fishing category tree"
+                this.category = CommandCategory.DEVELOPER_TEST
                 callback { printTree() }
             }
-        }
-
-        @HandleEvent
-        fun onGuiRender(event: GuiRenderEvent.GuiOverlayRenderEvent) {
-            Position(200, 100).renderString("Fishing Category: ${activeCategory?.internalName ?: "None"}", posLabel = "Fishing Category")
         }
 
         @HandleEvent
@@ -105,11 +154,25 @@ sealed class FishingCategory(val internalName: String, private val parent: Fishi
             activeCategory = currentCategory
         }
 
+        private fun MutableList<FishingCategory>.recursiveLoad(
+            clazz: KClass<*>,
+            parent: FishingCategory? = null,
+        ): FishingCategory? {
+            val newParent = clazz.objectInstance as? FishingCategory
+            addNotNull(newParent)
+            newParent?.parent = parent
+            clazz.nestedClasses.forEach {
+                val child = recursiveLoad(it, newParent)
+                newParent?.children?.addNotNull(child)
+            }
+            return newParent
+        }
 
-        fun printTree() {
+
+        private fun printTree() {
             fun printCategory(category: FishingCategory, level: Int) {
                 val prefix = "  ".repeat(level)
-                println("$prefix${category.internalName}")
+                ChatUtils.debug("$prefix${category.internalName}")
                 for (child in category.children) {
                     printCategory(child, level + 1)
                 }
---
+++ src/main/kotlin/com/github/itsempa/nautilus/features/dev/FishingCategoryDisplay.kt
@@ -0,0 +1,20 @@
+package com.github.itsempa.nautilus.features.dev
+
+import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.config.core.config.Position
+import at.hannibal2.skyhanni.events.GuiRenderEvent
+import at.hannibal2.skyhanni.utils.RenderUtils.renderString
+import com.github.itsempa.nautilus.data.categories.FishingCategory
+import com.github.itsempa.nautilus.modules.Module
+
+@Module(devOnly = true)
+object FishingCategoryDisplay {
+
+    private val pos = Position(100, 200)
+
+    @HandleEvent
+    fun onGuiRender(event: GuiRenderEvent.GuiOverlayRenderEvent) {
+        pos.renderString("Current Category: ${FishingCategory.activeCategory?.internalName ?: "None"}", posLabel = "Fishing Category")
+    }
+
+}
