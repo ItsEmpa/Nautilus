c8086f65bcc8b3377eb0018b0f3c1a924c0eaa3c
Empa <itsempa@users.noreply.github.com>
2025-05-17 11:59:09 +0200

    chore: add debug info to NautilusStorage

--- src/main/kotlin/com/github/itsempa/nautilus/data/NautilusStorage.kt
+++ src/main/kotlin/com/github/itsempa/nautilus/data/NautilusStorage.kt
@@ -6,6 +6,7 @@ import at.hannibal2.skyhanni.events.minecraft.ClientDisconnectEvent
 import com.github.itsempa.nautilus.Nautilus
 import com.github.itsempa.nautilus.config.storage.ProfileStorage
 import com.github.itsempa.nautilus.config.storage.Storage
+import com.github.itsempa.nautilus.events.NautilusDebugEvent
 import com.github.itsempa.nautilus.modules.Module
 
 @Module
@@ -16,14 +17,28 @@ object NautilusStorage {
     var profile: ProfileStorage = ProfileStorage()
         private set
 
+    var profileName: String? = null
+        private set
+
     @HandleEvent(priority = HandleEvent.HIGHEST)
     fun onProfileJoin(event: ProfileJoinEvent) {
         profile = storage.profileStorage.getOrPut(event.name, ::ProfileStorage)
+        profileName = event.name
     }
 
     @HandleEvent
     fun onDisconnect(event: ClientDisconnectEvent) {
         profile = ProfileStorage()
+        profileName = null
+    }
+
+    @HandleEvent
+    fun onDebug(event: NautilusDebugEvent) {
+        event.title("Nautilus Storage")
+        event.addIrrelevant(
+            "profileName" to profileName,
+            "profileStorage" to profile,
+        )
     }
 
 }
