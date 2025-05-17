1ae2d7c923068c91849b8c0c5eacf8a1401c33fe
Empa <itsempa@users.noreply.github.com>
2025-05-17 14:56:55 +0200

    chore: separated EnumMap and EnumSet functions into different files, added some more and improved them

--- src/main/kotlin/com/github/itsempa/nautilus/utils/NautilusCollectionUtils.kt
+++ src/main/kotlin/com/github/itsempa/nautilus/utils/NautilusCollectionUtils.kt
@@ -1,31 +1,7 @@
 package com.github.itsempa.nautilus.utils
 
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
-import java.util.EnumMap
-import java.util.EnumSet
 import kotlin.time.Duration
-
-inline fun <reified E : Enum<E>> enumSetOf(vararg elements: E): EnumSet<E> = EnumSet.copyOf(elements.toList())
-
-inline fun <reified E : Enum<E>> enumSetOf(): EnumSet<E> = EnumSet.noneOf(E::class.java)
-
-fun <E : Enum<E>> Set<E>.toEnumSet(): EnumSet<E> = EnumSet.copyOf(this)
-
-inline fun <reified K : Enum<K>, V> enumMapOf(): EnumMap<K, V> = EnumMap<K, V>(K::class.java)
-
-fun <K : Enum<K>, V> Map<K, V>.toEnumMap(): EnumMap<K, V> = EnumMap(this)
-
-inline fun <reified K : Enum<K>, V> fullEnumMapOf(defaultValue: V): EnumMap<K, V> {
-    return buildMap {
-        for (enum in enumValues<K>()) put(enum, defaultValue)
-    }.toEnumMap()
-}
-
-inline fun <reified K : Enum<K>, V> fullEnumMapOf(defaultValue: () -> V): EnumMap<K, V> {
-    return buildMap {
-        for (enum in enumValues<K>()) put(enum, defaultValue())
-    }.toEnumMap()
-}
 
 fun <T> MutableList<T>.removeFirstMatches(condition: (T) -> Boolean): T? {
     val indexOf = indexOfFirst(condition)
